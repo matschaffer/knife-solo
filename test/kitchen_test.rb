2424a20ae5e1d1a37916e3ad48ec9c8c43a3d587
Mat Schaffer <mat@schaffer.me>
2011-07-29 23:10:11 -0700

    Cleaner kitchen test.

--- test/kitchen_test.rb
+++ test/kitchen_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
+require 'chef/knife/kitchen'
+
 class KitchenTest < TestCase
   def test_produces_folders
     Dir.chdir("/tmp") do
-      system("knife", "kitchen", "testkitchen")
+      command("testkitchen").run
       assert File.exist?("testkitchen/nodes")
     end
   end
 
   def teardown
     FileUtils.rm_r("/tmp/testkitchen")
+  end
+
+  def command(*args)
+    Chef::Knife::Kitchen.load_deps
+    Chef::Knife::Kitchen.new(args)
   end
 end
