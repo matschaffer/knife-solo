b143ae290a91bb4aecdf833864d82e305663c45f
retr0h <john@dewey.ws>
2011-09-30 22:56:59 -0700

    Added proper path to roles

--- lib/chef/knife/kitchen.rb
+++ lib/chef/knife/kitchen.rb
@@ -21,7 +21,7 @@ class Chef
             data_bag_path   "/tmp/chef-solo/data_bags"
             cookbook_path   [ "/tmp/chef-solo/site-cookbooks",
                               "/tmp/chef-solo/cookbooks" ]
-            role_path       "/tmp/roles"
+            role_path       "/tmp/chef-solo/roles"
           RUBY
         end
       end
