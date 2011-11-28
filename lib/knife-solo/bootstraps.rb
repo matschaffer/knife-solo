b56d20a9fdd37b452e4ecfc5d883fbf4e4d12d54
nat <nat.lownes@gmail.com>
2011-11-28 12:25:34 -0500

    adding support for OSX 10.5 and 10.6 still requires 10.5 and 10.6 machines have XCode installed
    did a good chunk of refactoring around
    the initial chef bootstrapping that was happening in lib/chef/knife/prepare.rb
    - added KnifeSolo::Bootstraps with an eye toward keeping OS specific
      bootstrap jawns in different classes
    - OS specific bootstraps inherit from KnifeSolo::Bootstraps::Base

---
+++ lib/knife-solo/bootstraps.rb
@@ -0,0 +1,90 @@
+class OperatingSystemNotSupportedError < StandardError ; end
+
+module KnifeSolo
+  module Bootstraps
+    class OperatingSystemNotImplementedError < StandardError 
+    end
+
+    def self.class_exists_for?(os_name)
+      begin
+        true if self.class_for_operating_system(os_name).class == Class
+      rescue => exception
+        false
+      end
+    end
+
+    def self.class_for_operating_system(os_name)
+      begin
+        os_class_name = os_name.gsub(/\s/,'')
+        eval("KnifeSolo::Bootstraps::#{os_class_name}")
+      rescue
+        raise OperatingSystemNotImplementedError.new("#{os_name} not implemented.  Feel free to add a bootstrap implementation in KnifeSolo::Bootstraps::#{os_class_name}")
+      end
+    end
+
+    module Delegates
+      def run_command(cmd)
+        prepare.run_command(cmd)
+      end
+
+      def ui
+        prepare.ui
+      end
+
+      def prepare
+        @prepare
+      end
+    end #Delegates
+
+    module InstallCommands
+
+      def bootstrap!
+        run_pre_bootstrap_checks()
+        send("#{distro[:type]}_install")
+      end
+
+      def distro
+        raise "implement distro detection for #{self.class.name}"
+      end
+
+      def gem_packages
+        raise "implement gem packages for #{self.class.name}"
+      end
+
+      def http_client_get_url(url)
+        "wget #{url}"
+      end
+
+      def gem_install
+        ui.msg "Installing rubygems from source..."
+        release = "rubygems-1.8.10"
+        file = "#{release}.tgz"
+        url = "http://production.cf.rubygems.org/rubygems/#{file}"
+        run_command(http_client_get_url(url))
+        run_command("tar zxf #{file}")
+        run_command("cd #{release} && sudo ruby setup.rb --no-format-executable")
+        run_command("sudo rm -rf #{release} #{file}")
+        run_command("sudo gem install --no-rdoc --no-ri #{gem_packages().join(' ')}")
+      end
+    end #InstallCommands
+
+    class Base
+      include KnifeSolo::Bootstraps::Delegates
+      include KnifeSolo::Bootstraps::InstallCommands
+
+      def initialize(prepare)
+        # instance of Chef::Knife::Prepare
+        @prepare = prepare
+      end
+
+      def run_pre_bootstrap_checks ; end
+      # run right before we run #{distro[:type]}_install method
+      # barf out here if need be
+    end
+    
+  end # Bootstraps
+end
+
+
+# bootstrap classes for different OSes
+Dir[File.dirname(__FILE__) + '/bootstraps/*.rb'].each {|p| require p}
---
+++ lib/knife-solo/bootstraps/linux.rb
@@ -0,0 +1,102 @@
+module KnifeSolo::Bootstraps
+  class Linux < Base
+
+    def issue
+      prepare.run_command("cat /etc/issue").stdout.strip
+    end
+
+    def package_list
+      @packages.join(' ')
+    end
+
+    def gem_packages
+      ['ruby-shadow','chef']
+    end
+
+    def http_client_get_url(url)
+      "wget #{url}"
+    end
+
+    def zypper_gem_install
+      ui.msg("Installing required packages...")
+      run_command("sudo zypper --non-interactive install ruby-devel make gcc rsync")
+      gem_install
+    end
+
+    def emerge_gem_install
+      ui.msg("Installing required packages...")
+      run_command("sudo USE='-test' ACCEPT_KEYWORDS='~amd64' emerge -u chef")
+    end
+
+    def add_yum_repos
+      repo_url = "http://rbel.co/"
+      if distro[:version] == "RHEL5"
+        repo_path = "/rbel5"
+      else
+        repo_path = "/rbel6"
+      end
+      installed = "is already installed"
+      result = run_command("#{sudo} rpm -Uvh #{repo_url}#{repo_path}")
+      raise result.stderr_or_stdout unless result.success? || result.stdout.match(installed)
+    end
+
+    def yum_install
+      ui.msg("Installing required packages...")
+      add_yum_repos
+      @packages = %w(rubygem-chef rsync)
+      run_command("#{sudo} yum -y install #{package_list}")
+    end
+
+    def debian_gem_install
+      ui.msg "Updating apt caches..."
+      run_command("sudo apt-get update")
+
+      ui.msg "Installing required packages..."
+      @packages = %w(ruby ruby-dev libopenssl-ruby irb
+                     build-essential wget ssl-cert rsync)
+      run_command <<-BASH
+        sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install #{package_list}
+      BASH
+
+      gem_install
+    end
+
+    def distro
+      return @distro if @distro
+      @distro = case issue
+      when %r{Debian GNU/Linux 5}
+        {:type => "debian_gem", :version => "lenny"}
+      when %r{Debian GNU/Linux 6}
+        {:type => "debian_gem", :version => "squeeze"}
+      when %r{Debian GNU/Linux wheezy}
+        {:type => "debian_gem", :version => "wheezy"}
+      when %r{Ubuntu}
+        version = run_command("lsb_release -cs").stdout.strip
+        {:type => "debian_gem", :version => version}
+      when %r{CentOS.*? 5}
+        {:type => "yum", :version => "RHEL5"}
+      when %r{CentOS.*? 6}
+        {:type => "yum", :version => "RHEL6"}
+      when %r{Red Hat Enterprise.*? 5}
+        {:type => "yum", :version => "RHEL5"}
+      when %r{Red Hat Enterprise.*? 6}
+        {:type => "yum", :version => "RHEL6"}
+      when %r{Scientific Linux.*? 5}
+        {:type => "yum", :version => "RHEL5"}
+      when %r{Scientific Linux.*? 6}
+        {:type => "yum", :version => "RHEL6"}
+      when %r{SUSE Linux Enterprise Server 11 SP1}
+        {:type => "zypper_gem", :version => "SLES11"}
+      when %r{openSUSE 11.4}
+        {:type => "zypper_gem", :version => "openSUSE"}
+      when %r{This is \\n\.\\O (\\s \\m \\r) \\t}
+        {:type => "gentoo_gem", :version => "Gentoo"}
+      else
+        raise "Distro not recognized from looking at /etc/issue. Please fork https://github.com/matschaffer/knife-solo and add support for your distro."
+      end
+      Chef::Log.debug("Distro detection yielded: #{@distro}")
+      @distro
+    end #issue
+
+  end
+end
