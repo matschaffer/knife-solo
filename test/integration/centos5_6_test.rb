734762233fe3e67a2e615c3de1b58c82a055eb84
Mat Schaffer <mat@schaffer.me>
2011-12-09 14:49:06 -0500

    CentOS 5.6 test

---
+++ test/integration/centos5_6_test.rb
@@ -0,0 +1,13 @@
+require 'integration_helper'
+
+class Centos5_6Test < IntegrationTest
+  def user
+    "root"
+  end
+
+  def image_id
+    "ami-3fe42456"
+  end
+
+  include IntegrationTest::BasicPrepareAndCook
+end
