59bb658905ebed91a684e299f70d97baef95fc1b
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-11 21:56:06 +0100

    Tweak the test to avoid running a process for real

--- test/tools_test.rb
+++ test/tools_test.rb
@@ -5,13 +5,14 @@ class ToolsTest < TestCase
   include KnifeSolo::Tools
 
   def test_raises_exception_if_error
-    assert_raises CommandFailedError, /funky/ do
-      system! "funkynonexistingcommand"
+    # Cannot mock Kernel.system with mocha
+    self.expects(:system).with('mycommand').returns(false)
+    assert_raises CommandFailedError, /mycommand/ do
+      system! "mycommand"
     end
   end
 
   def test_does_not_raise_if_system_successful
-    # Cannot mock Kernel.system with mocha
     self.expects(:system).with('mycommand').returns(true)
     system! 'mycommand'
   end
