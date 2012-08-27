1e5d532ac316e8cb2fd5ada14ba8e0d7b86a2b81
Mat Schaffer <mat@schaffer.me>
2012-08-26 22:16:27 -0400

    Somewhat cleaner provisioner

--- spec/integration/support/knife_solo_provisioner.rb
+++ spec/integration/support/knife_solo_provisioner.rb
@@ -3,12 +3,25 @@ class KnifeSoloProvisioner < Vagrant::Provisioners::Base
     env[:vm].ssh.info
   end
 
+  def connection_arguments
+    [ "#{ssh_info[:username]}@#{ssh_info[:host]}",
+      "-p", ssh_info[:port].to_s,
+      "-i", ssh_info[:private_key_path] ]
+  end
+
+  def knife(command)
+    arguments = ["knife", command] + connection_arguments
+    system *arguments
+  end
+
+  def kitchen
+    File.expand_path("../..", __FILE__)
+  end
+
   def provision!
-    Dir.chdir(ENV['VAGRANT_CWD']) do
-      system "knife", "prepare",
-        "#{ssh_info[:username]}@#{ssh_info[:host]}",
-        "-p", ssh_info[:port].to_s,
-        "-i", ssh_info[:private_key_path]
+    Dir.chdir(kitchen) do
+      knife "prepare"
+      knife "cook"
     end
   end
 end
