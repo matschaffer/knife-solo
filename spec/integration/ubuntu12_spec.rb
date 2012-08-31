1ae39ea4fcd43f64426c57b23e175506833e6d88
Mat Schaffer <mat@schaffer.me>
2012-08-31 11:04:45 -0400

    Make sure we fail on a bad chef run.

--- spec/integration/ubuntu12_spec.rb
+++ spec/integration/ubuntu12_spec.rb
@@ -4,4 +4,8 @@ describe 'ubuntu12' do
   it 'can run a recipe with tests' do
     provision(run_list: [ "recipe[minitest-handler]", "recipe[apache2]" ]).should == true
   end
+
+  it 'fails on a bad run' do
+    provision(run_list: [ "recipe[no-recipe-here]" ]).should == false
+  end
 end
