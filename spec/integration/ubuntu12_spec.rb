3664adcd9e2b17d4dfeb0df355b53152be787c70
Mat Schaffer <mat@schaffer.me>
2012-09-22 01:04:15 -0400

    Apache mini tests are broken
    Thinking I should just make my own cookbook for this and also avoid librarian that way.

--- spec/integration/ubuntu12_spec.rb
+++ spec/integration/ubuntu12_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe 'ubuntu12' do
   it 'can run a recipe with tests' do
-    provision(run_list: [ "recipe[minitest-handler]", "recipe[apache2]" ]).should == true
+    provision(run_list: [ "recipe[apache2]" ]).should == true
   end
 
   it 'fails on a bad run' do
