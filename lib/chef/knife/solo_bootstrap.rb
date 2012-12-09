c17de155ac79273e553750a5cb95a2101b79e081
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2012-12-09 15:20:51 -0300

    Add validation to solo bootstrap class
    Otherwise the error message will display help message for an underlying
    subcommand.

--- lib/chef/knife/solo_bootstrap.rb
+++ lib/chef/knife/solo_bootstrap.rb
@@ -2,10 +2,17 @@ require 'chef/knife'
 require 'chef/knife/solo_cook'
 require 'chef/knife/solo_prepare'
 
+require 'knife-solo/kitchen_command'
+require 'knife-solo/ssh_command'
+
 class Chef
   class Knife
     class SoloBootstrap < Knife
+      include KnifeSolo::KitchenCommand
+      include KnifeSolo::SshCommand
+
       deps do
+        KnifeSolo::SshCommand.load_deps
         SoloPrepare.load_deps
         SoloCook.load_deps
       end
@@ -17,12 +24,19 @@ class Chef
       [:sync_only, :why_run].each { |opt| option opt, SoloCook.options[opt] }
 
       def run
+        validate!
+
         prepare = command_with_same_args(SoloPrepare)
         prepare.run
 
         cook = command_with_same_args(SoloCook)
         cook.config[:skip_chef_check] = true
         cook.run
+      end
+
+      def validate!
+        validate_first_cli_arg_is_a_hostname!
+        validate_kitchen!
       end
 
       def command_with_same_args(klass)
