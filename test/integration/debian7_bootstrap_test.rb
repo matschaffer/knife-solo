f299a6704cc9d2cd4234ce0f09cf68fe22b276ea
Mat Schaffer <mat@schaffer.me>
2013-01-16 11:25:52 -0500

    Adding Debian 7 support

---
+++ test/integration/debian7_bootstrap_test.rb
@@ -0,0 +1,21 @@
+require 'integration_helper'
+
+class Debian7BootstrapTest < IntegrationTest
+  def user
+    "admin"
+  end
+
+  def image_id
+    "ami-0ba62d62"
+  end
+
+  def prepare_server
+    # Do nothing as `solo bootstrap` will do everything
+  end
+
+  def default_apache_message
+    /iIt works!/
+  end
+
+  include Apache2Bootstrap
+end
