87642afc5312a7ec9017c08f02aebb35163bc130
Mat Schaffer <mat@schaffer.me>
2013-02-09 23:15:52 -0500

    A test case that demonstrates #86
    First thing's first.

---
+++ test/support/cache_using_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'cache_using_cookbook'
+maintainer       'Mat Schaffer'
+maintainer_email 'mat@schaffer.me'
+license          'MIT'
+description      'Writes some data to the cache path.'
+long_description 'This helps ensure that we can do this multiple times regardless of what user initiates the run.'
+version          '0.1.0'
