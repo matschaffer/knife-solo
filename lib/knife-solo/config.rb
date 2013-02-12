ab610f5faa81c87d9743c4a5a28503bde9d9a35b
Mat Schaffer <mat@schaffer.me>
2013-02-11 22:40:40 -0500

    A KnifeSolo::Config to encapsulate configuration discovery

---
+++ lib/knife-solo/config.rb
@@ -0,0 +1,39 @@
+require 'chef/config'
+
+module KnifeSolo
+  # Encapsulates some logic for checking and extracting
+  # path configuration from the structure of the
+  # current kitchen
+  class Config
+    def solo_path
+      Chef::Config.knife[:solo_path]
+    end
+
+    def chef_path
+      solo_path || './chef-solo'
+    end
+
+    def cookbook_path
+      if using_custom_solorb?
+        Chef::Config.from_file('solo.rb')
+        Array(Chef::Config.cookbook_path).first
+      else
+        chef_path + '/cookbooks'
+      end
+    end
+
+    def patch_path
+      cookbook_path + "/chef_solo_patches/libraries"
+    end
+
+    def using_custom_solorb?
+      File.exist?('solo.rb')
+    end
+
+    def validate!
+      raise Error, "You have a solo.rb file, but knife[:solo_path] is not set. You probably need to delete solo.rb unless you've customized it. See https://github.com/matschaffer/knife-solo/wiki/Upgrading-to-0.2.0 for more information." if using_custom_solorb? && solo_path.nil?
+    end
+
+    class Error < StandardError; end
+  end
+end
