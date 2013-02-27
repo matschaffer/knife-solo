fbe84f4c00f3bbfdee03f84556a351f838cf7af3
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-02-27 17:05:37 -0300

    Move Linux specific bootstrap methods to appropriate subclass

--- lib/knife-solo/bootstraps/linux.rb
+++ lib/knife-solo/bootstraps/linux.rb
@@ -42,6 +42,24 @@ module KnifeSolo::Bootstraps
       gem_install
     end
 
+    def debianoid_omnibus_install
+      omnibus_install
+      # Update to avoid out-of-date package caches
+      run_command("sudo apt-get update")
+      # Make sure we have rsync on builds that don't include it by default
+      # (for example linode's ubuntu 10.04 images)
+      run_command("sudo apt-get -y install rsync")
+    end
+
+    def yum_omnibus_install
+      omnibus_install
+      # Avoid rsync not being found in package cache.
+      run_command("sudo yum clean all")
+      # Make sure we have rsync on builds that don't include it by default
+      # (for example Scientific Linux minimal, CentOS minimal)
+      run_command("sudo yum -y install rsync")
+    end
+
     def distro
       return @distro if @distro
       @distro = case issue
