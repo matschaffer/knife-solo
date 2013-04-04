e8ddd2761e258bf452aa804040924d3f976212c0
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-04-04 00:42:35 -0300

    Ensure patch_cookbooks path is a string

--- lib/chef/knife/solo_cook.rb
+++ lib/chef/knife/solo_cook.rb
@@ -100,7 +100,7 @@ class Chef
       end
 
       def cookbook_paths
-        Chef::Config.cookbook_path + [KnifeSolo.resource('patch_cookbooks')]
+        Chef::Config.cookbook_path + [KnifeSolo.resource('patch_cookbooks').to_s]
       end
 
       def nodes_path
