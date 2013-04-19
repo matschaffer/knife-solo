f315fbaa87248dfef45a6c633cfabeae940b4b0c
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-04-19 19:38:05 -0300

    Use Berkshelf in apache2_bootstrap integration test

--- test/integration/cases/apache2_bootstrap.rb
+++ test/integration/cases/apache2_bootstrap.rb
@@ -6,8 +6,15 @@ require $base_dir.join('integration', 'cases', 'apache2_cook')
 module Apache2Bootstrap
   include Apache2Cook
 
+  def write_berksfile
+    File.open('Berksfile', 'w') do |f|
+      f.puts "site :opscode"
+      f.puts "cookbook 'apache2'"
+    end
+  end
+
   def test_apache2
-    write_cheffile
+    write_berksfile
     assert_subcommand "bootstrap --run-list=recipe[apache2]"
     assert_match default_apache_message, http_response
   end
