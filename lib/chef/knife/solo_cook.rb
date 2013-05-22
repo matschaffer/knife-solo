18c1f2f8818564fd75916332f1c2220088cb2ef5
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-05-22 00:14:47 -0300

    Upload all node json files and configure them for chef-solo-search
    Future chef-solo-search versions will use `nodes_path` when searching
    nodes. So upload them all and set the configuration accordingly.

--- lib/chef/knife/solo_cook.rb
+++ lib/chef/knife/solo_cook.rb
@@ -107,6 +107,7 @@ class Chef
           upload_to_provision_path(path, "/cookbooks-#{i + 1}", 'cookbook_path')
         end
         upload_to_provision_path(node_config, 'dna.json')
+        upload_to_provision_path(nodes_path, 'nodes')
         upload_to_provision_path(:role_path, 'roles')
         upload_to_provision_path(:data_bag_path, 'data_bags')
         upload_to_provision_path(:encrypted_data_bag_secret, 'data_bag_key')
