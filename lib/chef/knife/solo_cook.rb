ac1908752f105ea4be28e804988594440b38480b
DQNEO <dqneoo@gmail.com>
2014-09-10 00:43:48 +0900

    display cmd string on running chef-solo command
    Showing what's happing is very important for newbies.

--- lib/chef/knife/solo_cook.rb
+++ lib/chef/knife/solo_cook.rb
@@ -303,6 +303,8 @@ class Chef
         cmd << " -W" if config[:why_run]
         cmd << " -o #{config[:override_runlist]}" if config[:override_runlist]
 
+        ui.msg cmd
+
         result = stream_command cmd
         raise "chef-solo failed. See output above." unless result.success?
       end
