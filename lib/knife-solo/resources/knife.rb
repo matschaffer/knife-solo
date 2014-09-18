9f1a98592f271a80f6905efc1e6f08a7c965daaf
Kevin McAllister <kevin@mcallister.ws>
2014-09-18 13:59:17 -0400

    swap cookbooks in path to benefit cookbook create
    this causes `knife cookbook create` to use `site-cookbooks` instead of `cookbooks`

--- lib/knife-solo/resources/knife.rb
+++ lib/knife-solo/resources/knife.rb
@@ -1,4 +1,4 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
+cookbook_path    ["site-cookbooks", "cookbooks"]
 node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
