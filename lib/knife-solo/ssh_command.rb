754951a146a9fd7d9118554075578f5fd000d47f
Braulio Bhavamitra <braulio@eita.org.br>
2015-06-08 12:41:00 -0300

    Use sudo -i as default sudo command

--- lib/knife-solo/ssh_command.rb
+++ lib/knife-solo/ssh_command.rb
@@ -70,7 +70,8 @@ module KnifeSolo
 
         option :sudo_command,
           :long        => '--sudo-command SUDO_COMMAND',
-          :description => 'The command to use instead of sudo for admin privileges'
+          :description => 'The command to use instead of sudo for admin privileges',
+          default: 'sudo -i'
 
         option :host_key_verify,
           :long => "--[no-]host-key-verify",
