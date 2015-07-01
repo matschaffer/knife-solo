a51f5850677ea447629321256ee34543b4f56821
Mat Schaffer <mat@schaffer.me>
2015-07-01 14:18:11 +0900

    Change fileutils call based on feedback

--- lib/knife-solo/ssh_command.rb
+++ lib/knife-solo/ssh_command.rb
@@ -216,7 +216,7 @@ module KnifeSolo
 
     def ssh_control_path
       dir = File.join(ENV['HOME'], '.chef', 'knife-solo-sockets')
-      FileUtils.mkpath(dir) unless File.exist?(dir)
+      FileUtils.mkdir_p(dir)
       File.join(dir, '%h')
     end
 
