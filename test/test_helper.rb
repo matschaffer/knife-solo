b698769ff98a0a4823067599d1f07a85c981f4ed
Mat Schaffer <mat@schaffer.me>
2015-11-22 07:21:42 +0900

    Move ShellOut fix to test helper with version constraint

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
 
+# Avoids uninitialized constant Chef::Mixin::ShellOut - see https://github.com/chef/chef/pull/4153
+require 'chef/version'
+if Gem::Requirement.new('>= 12.5.1').satisfied_by? Gem::Version.new(Chef::VERSION)
+  require 'chef/mixin/shell_out'
+end
+
 require 'coveralls'
 Coveralls.wear!
 
