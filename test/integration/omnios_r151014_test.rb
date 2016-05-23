f67216036524d5c8a4e044aef7d2cedf560fefea
Mat Schaffer <mat@schaffer.me>
2016-05-23 10:11:22 +0900

    Update OmniOS to latest LTS

--- test/integration/omnios_r151014_test.rb
+++ test/integration/omnios_r151014_test.rb
@@ -1,12 +1,12 @@
 require 'integration_helper'
 
-class OmniOSr151004Test < IntegrationTest
+class OmniOSr151014Test < IntegrationTest
   def user
     "root"
   end
 
   def image_id
-    "ami-4e2c9727"
+    "ami-79ee1b14"
   end
 
   include EmptyCook
