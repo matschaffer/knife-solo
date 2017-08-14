2904233813d8ca43ed40d2606491931fdcc18904
Issa Ashwash <issa@area1security.com>
2017-08-14 14:58:22 -0700

    Uses sudo when running clean

--- test/solo_clean_test.rb
+++ test/solo_clean_test.rb
@@ -8,7 +8,13 @@ class SoloCleanTest < TestCase
 
   def test_removes_provision_path
     cmd = command('somehost', '--provisioning-path=/foo/bar')
-    cmd.expects(:run_command).with('rm -rf /foo/bar').returns(SuccessfulResult.new)
+    cmd.expects(:run_command).with('sudo rm -rf /foo/bar').returns(SuccessfulResult.new)
+    cmd.run
+  end
+
+  def test_removes_with_custom_sudo
+    cmd = command('somehost', '--sudo-command=/usr/some/sudo')
+    cmd.expects(:run_command).with('/usr/some/sudo rm -rf ~/chef-solo').returns(SuccessfulResult.new)
     cmd.run
   end
 
